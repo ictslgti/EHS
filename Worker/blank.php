b0152edfff8f72d4a5ad47df7767dbd68e045de4
sumanan24 <sumanan24@gmail.com>
2023-06-20 09:26:35 +0530

    edit

---
+++ Worker/blank.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Focus Admin Dashboard</title>
+
+    <?php include_once('../script.php');?>
+</head>
+
+<body>
+
+    
+<?php include_once('nav.php');?>
+
+<?php include_once('head.php');?>
+
+
+
+   
+
+
+
+    <div class="content-wrap">
+        <div class="main">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12 p-r-0 title-margin-right">
+                        <div class="card">
+                            <div class="card-header">
+                               <h5> Employees</h5>
+                            </div>
+                            <div class="card-body">
+                           <!-- your content start  -->
+                            Welcome to Hire Worker!
+
+
+                           <!-- your content finish  -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    <?php include_once('../navscript.php');?>
+</body>
+
+</html>
---
+++ script.php
@@ -0,0 +1,50 @@
+<!-- ================= Favicon ================== -->
+    <!-- Standard -->
+    <link rel="shortcut icon" href="http://placehold.it/64.png/000/fff">
+    <!-- Retina iPad Touch Icon-->
+    <link rel="apple-touch-icon" sizes="144x144" href="http://placehold.it/144.png/000/fff">
+    <!-- Retina iPhone Touch Icon-->
+    <link rel="apple-touch-icon" sizes="114x114" href="http://placehold.it/114.png/000/fff">
+    <!-- Standard iPad Touch Icon-->
+    <link rel="apple-touch-icon" sizes="72x72" href="http://placehold.it/72.png/000/fff">
+    <!-- Standard iPhone Touch Icon-->
+    <link rel="apple-touch-icon" sizes="57x57" href="http://placehold.it/57.png/000/fff">
+
+    <!-- Toastr -->
+    <link href="css/lib/toastr/toastr.min.css" rel="stylesheet">
+    <!-- Sweet Alert -->
+    <link href="css/lib/sweetalert/sweetalert.css" rel="stylesheet">
+    <!-- Range Slider -->
+    <link href="css/lib/rangSlider/ion.rangeSlider.css" rel="stylesheet">
+    <link href="css/lib/rangSlider/ion.rangeSlider.skinFlat.css" rel="stylesheet">
+    <!-- Bar Rating -->
+    <link href="css/lib/barRating/barRating.css" rel="stylesheet">
+    <!-- Nestable -->
+    <link href="css/lib/nestable/nestable.css" rel="stylesheet">
+    <!-- JsGrid -->
+    <link href="css/lib/jsgrid/jsgrid-theme.min.css" rel="stylesheet" />
+    <link href="css/lib/jsgrid/jsgrid.min.css" type="text/css" rel="stylesheet" />
+    <!-- Datatable -->
+    <link href="css/lib/datatable/dataTables.bootstrap.min.css" rel="stylesheet" />
+    <link href="css/lib/data-table/buttons.bootstrap.min.css" rel="stylesheet" />
+    <!-- Calender 2 -->
+    <link href="css/lib/calendar2/pignose.calendar.min.css" rel="stylesheet">
+    <!-- Weather Icon -->
+    <link href="css/lib/weather-icons.css" rel="stylesheet" />
+    <!-- Owl Carousel -->
+    <link href="css/lib/owl.carousel.min.css" rel="stylesheet" />
+    <link href="css/lib/owl.theme.default.min.css" rel="stylesheet" />
+    <!-- Select2 -->
+    <link href="css/lib/select2/select2.min.css" rel="stylesheet">
+    <!-- Chartist -->
+    <link href="css/lib/chartist/chartist.min.css" rel="stylesheet">
+    <!-- Calender -->
+    <link href="css/lib/calendar/fullcalendar.css" rel="stylesheet" />
+
+    <!-- Common -->
+    <link href="css/lib/font-awesome.min.css" rel="stylesheet">
+    <link href="css/lib/themify-icons.css" rel="stylesheet">
+    <link href="css/lib/menubar/sidebar.css" rel="stylesheet">
+    <link href="css/lib/bootstrap.min.css" rel="stylesheet">
+    <link href="css/lib/helper.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
