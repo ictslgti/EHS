6247382d95cc2062390ef7f4b3c934b9920a791e
krishnna4054 <krish4054123@gmail.com>
2023-09-06 14:29:04 +0100

    edit

--- Customer/blank.php
+++ Customer/blank.php
@@ -1,3 +1,15 @@
+<html lang="en">
+<?php 
+$con = mysqli_connect('localhost', 'root', '','hire_work');
+
+// Check connection
+if ($con->connect_error) {
+  die("Connection failed: " . $con->connect_error);
+}
+
+session_start();
+$user_count=null;
+?>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -361,4 +373,79 @@
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div><?php
+$title = 'admin | Dashbort.php';
+include_once('head.php');
+?>
+
+<body>
+    <div class="container">
+        <?php include_once('nav.php'); ?>
+        <?php
+      if (isset($_SESSION['email'])) {
+        $email = $_SESSION['email'];
+    } else {
+        // Handle the case when 'email' key is not set in the session.
+    }
+    
+        $sql_user = "SELECT count(id) as countid from worker";
+        $result_user = mysqli_query($con, $sql_user);
+        $user = mysqli_fetch_assoc($result_user);
+        $user_count = $user['countid'];
+
+
+        if (isset($_POST['post']) && !empty($_POST['body'])) {
+            $body = $_POST['body'];
+            $sql_post = "INSERT INTO `posts` (`body`, `user_id`) VALUES ('$body', '$user_id')";
+            if (mysqli_query($con, $sql_post)) {
+                echo '<div class="alert alert-success" role="alert">
+                        Successfully Posted!
+                    </div>';
+            }
+        }
+
+        if (isset($_GET['delete'])) {
+            //TO DO
+            $post_id = $_GET['delete'];
+
+            $sql_post = "SELECT `user_id` FROM `posts` WHERE `id` = $post_id";
+            $result_post = mysqli_query($con, $sql_post);
+            $row = mysqli_fetch_assoc($result_post);
+            if (mysqli_num_rows($result_post) == 1 && $row['user_id'] == $user_id) {
+                $sql_delete = "DELETE FROM `posts` WHERE `posts`.`id` = $post_id";
+                if (mysqli_query($con, $sql_delete)) {
+                    echo '<div class="alert alert-success" role="alert">
+                        Successfully Deleted!
+                    </div>';
+                }
+            }
+        }
+        $post_body = null;
+        $post_id = null;
+        if (isset($_GET['edit'])) {
+            $post_id = $_GET['edit'];
+            $sql_post = "SELECT * FROM `posts` WHERE `id` = $post_id";
+            $result_post = mysqli_query($con, $sql_post);
+            $row_post = mysqli_fetch_assoc($result_post);
+            // var_dump(mysqli_num_rows($result_post));
+            if (mysqli_num_rows($result_post) == 1 ) {
+                $post_body = $row_post['body'];
+            }
+        }
+        if (isset($_POST['update']) && !empty($_POST['body'])) {
+            $body = $_POST['body'];
+            $sql_post = "UPDATE `posts` SET `body` = '$body' WHERE `posts`.`id` = $post_id";
+            if (mysqli_query($con, $sql_post)) {
+                echo '<div class="alert alert-success" role="alert">
+                                Successfully Updated!
+                            </div>';
+            }
+        }
+
+
+        ?>
+
+
+</body>
+
+</html>
--- Worker/blank.php
+++ Worker/blank.php
@@ -1,5 +1,15 @@
 <html lang="en">
+<?php 
+$con = mysqli_connect('localhost', 'root', '','hire_work');
 
+// Check connection
+if ($con->connect_error) {
+  die("Connection failed: " . $con->connect_error);
+}
+
+session_start();
+$user_count=null;
+?>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -30,6 +40,7 @@
                                     
                                         <marquee>
                                             <h3>
+                                                
                                             <p style="color:maroon;"> Welcome to Hire Worker!</h3>
                                         </marquee>
 
@@ -127,6 +138,72 @@
             }
         });
     </script>
+<?php
+$title = 'Worker | blank.php';
+include_once('head.php');
+?>
+
+<body>
+    <div class="container">
+        <?php include_once('nav.php'); ?>
+        <?php
+        $email = $_SESSION['email'];
+        $sql_user = "SELECT count(id) as countid from worker";
+        $result_user = mysqli_query($con, $sql_user);
+        $user = mysqli_fetch_assoc($result_user);
+        $user_count = $user['countid'];
+
+
+        if (isset($_POST['post']) && !empty($_POST['body'])) {
+            $body = $_POST['body'];
+            $sql_post = "INSERT INTO `posts` (`body`, `user_id`) VALUES ('$body', '$user_id')";
+            if (mysqli_query($con, $sql_post)) {
+                echo '<div class="alert alert-success" role="alert">
+                        Successfully Posted!
+                    </div>';
+            }
+        }
+
+        if (isset($_GET['delete'])) {
+            //TO DO
+            $post_id = $_GET['delete'];
+
+            $sql_post = "SELECT `user_id` FROM `posts` WHERE `id` = $post_id";
+            $result_post = mysqli_query($con, $sql_post);
+            $row = mysqli_fetch_assoc($result_post);
+            if (mysqli_num_rows($result_post) == 1 && $row['user_id'] == $user_id) {
+                $sql_delete = "DELETE FROM `posts` WHERE `posts`.`id` = $post_id";
+                if (mysqli_query($con, $sql_delete)) {
+                    echo '<div class="alert alert-success" role="alert">
+                        Successfully Deleted!
+                    </div>';
+                }
+            }
+        }
+        $post_body = null;
+        $post_id = null;
+        if (isset($_GET['edit'])) {
+            $post_id = $_GET['edit'];
+            $sql_post = "SELECT * FROM `posts` WHERE `id` = $post_id";
+            $result_post = mysqli_query($con, $sql_post);
+            $row_post = mysqli_fetch_assoc($result_post);
+            // var_dump(mysqli_num_rows($result_post));
+            if (mysqli_num_rows($result_post) == 1 ) {
+                $post_body = $row_post['body'];
+            }
+        }
+        if (isset($_POST['update']) && !empty($_POST['body'])) {
+            $body = $_POST['body'];
+            $sql_post = "UPDATE `posts` SET `body` = '$body' WHERE `posts`.`id` = $post_id";
+            if (mysqli_query($con, $sql_post)) {
+                echo '<div class="alert alert-success" role="alert">
+                                Successfully Updated!
+                            </div>';
+            }
+        }
+
+
+        ?>
 
 
 </body>
