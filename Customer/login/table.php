922cd5e8938269d2ea1785dd8e85b13ed16ef0af
danuJan7 <danujan07@gmail.com>
2023-09-06 09:47:42 +0530

    change

--- Customer/login/table.php
+++ Customer/login/table.php
@@ -1,3 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+</head>
+
+<body>
+    <link rel="stylesheet" href="style.css">
+    <h1 style="color: rgb(85, 223, 85);">
+        Register!!
+    </h1>
+    <h2>User Data Management</h2>
+
+    <div id="formContainer">
+        <label for="nameInput">
+            Customer Name:
+        </label>
+        <input type="text" id="nameInput" placeholder="Enter your name">
+        <label for="numberInput">
+            Phone Number:
+        </label>
+        <input type="text" id="numberInput" placeholder="Enter your mobile Number">
+        <label for="numberInput">
+            Street:
+        </label>
+        <input type="text" id="numberInput" placeholder=" Street">
+        <label for="numberInput">
+            City:
+        </label>
+        <input type="text" id="numberInput" placeholder=" City">
+        <label for="emailInput">
+            Email I'd:
+        </label>
+        <input type="email" id="emailInput" placeholder="Enter your email">
+
+
+        <button onclick="addData()">
+            Register
+        </button>
+    </div>
+    <table id="outputTable">
+        <tr>
+            <th>Customer Name</th>
+            <th>Phone Number</th>
+            <th>Street</th>
+            <th>City</th>
+            <th>Email</th>
+        </tr>
+    </table>
+
+
 <?php
 
 $name = $mobile = $street = $city = $email = "";
@@ -12,11 +64,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $email = $_POST["email"];
 
   
-    $servername = "your_server_name";
-    $username = "your_username";
-    $password = "your_password";
-    $dbname = "your_database_name";
-
+ 
     
     $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -44,5 +92,5 @@ function editData() {
 }
 
 function deleteData() {
- 
+}
 ?>
